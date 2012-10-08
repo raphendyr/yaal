cbcc9383262ba4b66674f01ef5d49432feb378df
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2012-10-08 22:22:48 +0300

    Make basic_led_blink a real example

--- examples/basic_led_blink/main.cpp
+++ examples/basic_led_blink/main.cpp
@@ -1,4 +1,5 @@
 #include "yaal/io.hh"
+#include <util/delay.h>
 
 using namespace yaal;
 
@@ -8,7 +9,9 @@ int main(void) {
 
     while (1) {
         led.set();
+        _delay_ms(800);
         led.clear();
+        _delay_ms(800);
     }
 
 }
