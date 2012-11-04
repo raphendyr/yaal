cd15666459276598a6d8f2aca3e0f5e7d7f206aa
Jaakko Kantojärvi <jaakko@darkeagle.dy.fi>
2012-11-04 15:15:13 +0200

    avr-gcc 4.5.3 is not enough

--- requirements.hh
+++ requirements.hh
@@ -11,15 +11,9 @@
 #  error  "GNU gcc is required. If this source can be build using other tools, please submit pull request."
 #endif
 
-// check gcc is 4.6.0 and if 4.5.0 show warning
-#if __GNUC__ < 5
-#  if  __GNUC__ == 4 && __GNUC_MINOR__ > 4
-#    if __GNUC_MINOR__ == 5
-#      warning  "GNU 4.5 is not ntested. Please edit this and submit pull request, if 4.5 is ok."
-#    endif
-#  else
-#    error  "GNU 4.6+ is required. 4.5+ might work."
-#  endif
+// check gcc is atleast 4.6.0
+#if __GNUC__ < 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ < 6 )
+#  error  "GNU 4.6+ is required."
 #endif
 
 #endif
