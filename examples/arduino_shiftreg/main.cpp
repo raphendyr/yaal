1d8475b0c4c9785bf4189c6900c8cb1d178a1aba
Jaakko Kantojärvi <jaakko@darkeagle.dy.fi>
2012-11-24 22:35:14 +0200

    Update spi example

--- examples/arduino_shiftreg/main.cpp
+++ examples/arduino_shiftreg/main.cpp
@@ -2,20 +2,12 @@
 #define ARDUINO_teensy
 
 #include "yaal/arduino.hh"
-#include "yaal/devices/rgbled.hh"
 #include "yaal/communication/spi.hh"
+#include "yaal/setup_and_loop.hh"
 #include <util/delay.h>
 
 using namespace yaal;
 using namespace yaal::arduino;
-
-union uint16_u {
-    uint16_t value;
-    uint8_t arr[2];
-};
-
-
-BasicRGBLed<D1, D2, D0> rgb;
 
 typedef D21 Latch;
 typedef D18 Data;
@@ -23,41 +15,18 @@ typedef D15 Clock;
 
 SPI<Clock, Data, NullPin, Latch> spi;
 
-Latch latch;
-Data data;
-Clock clock;
-
-void write(uint16_t value) {
-    uint16_u v;
-    v.value = value;
-    rgb.red();
-    latch = true;
-    spi.write(v.arr[0]);
-    spi.write(v.arr[1]);
-    latch = false;
-    rgb.off();
+void setup() {
+    spi.setup();
 }
 
-
-
-int main(void) {
-
-    // initialize
-    rgb.setup();
-    spi.setup();
-
-
-    // loop
-    for (;;) {
-        uint16_t v = 7;
-        uint16_t d = 400;
-        for (uint8_t i = 0; i < 16; i++) {
-            write(v);
-            _delay_ms(d);
-            d -= 20;
-            v <<= 1;
-        }
+void loop() {
+    uint16_t value = 7;
+    for (uint8_t i = 0; i < 26; i++) {
+        spi.write(value);
+        if (i < 13)
+            value <<= 1;
+        else
+            value >>= 1;
+        _delay_ms(50);
     }
-
-    return 0;
 }
