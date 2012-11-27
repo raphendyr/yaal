a3bafed71e1d71ae458467bb46e17e044e7ce86b
Jaakko Kantojärvi <jaakko@darkeagle.dy.fi>
2012-11-28 00:21:20 +0200

    Add prototype for main.

--- core.hh
+++ core.hh
@@ -4,6 +4,9 @@
 /* cpu */
 #include "core/cpu.hh"
 
+/* prototypes main as no return */
+#include "core/main.hh"
+
 /* start and stop sections */
 #include "core/init.hh"
 #include "core/finish.hh"
---
+++ core/main.hh
@@ -0,0 +1,6 @@
+#ifndef __YAAL_CORE__MAIN__
+#define __YAAL_CORE__MAIN__ 1
+
+void main() __attribute__((noreturn));
+
+#endif
