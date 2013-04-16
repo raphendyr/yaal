718dff572990efad9ed1ef3b9d85a8075449977e
Ari Sundholm <asundhol@cs.hut.fi>
2013-04-16 21:43:11 +0300

    serials.hh: Move #ifdefs to beginning of line.

--- io/serials.hh
+++ io/serials.hh
@@ -7,21 +7,21 @@
 
 namespace yaal {
 
-    #ifdef UDR0
+#ifdef UDR0
     typedef internal::Serial<internal::Udr0, internal::Ucsr0a, internal::Ucsr0b, internal::Ucsr0c, internal::Ubrr0l, internal::Ubrr0h> Serial0;
-    #endif
+#endif
 
-    #ifdef UDR1
+#ifdef UDR1
     typedef internal::Serial<internal::Udr1, internal::Ucsr1a, internal::Ucsr1b, internal::Ucsr1c, internal::Ubrr1l, internal::Ubrr1h> Serial1;
-    #endif
+#endif
 
-    #ifdef UDR2
+#ifdef UDR2
     typedef internal::Serial<internal::Udr2, internal::Ucsr2a, internal::Ucsr2b, internal::Ucsr2c, internal::Ubrr2l, internal::Ubrr2h> Serial2;
-    #endif
+#endif
 
-    #ifdef UDR3
+#ifdef UDR3
     typedef internal::Serial<internal::Udr3, internal::Ucsr3a, internal::Ucsr3b, internal::Ucsr3c, internal::Ubrr3l, internal::Ubrr3h> Serial3;
-    #endif
+#endif
 }
 
 #endif
