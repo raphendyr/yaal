0a88e0ec7cf64f6069ef793e8ca23ccc4b7704a3
Ari Sundholm <asundhol@cs.hut.fi>
2013-04-16 12:15:15 +0300

    autounion: fix mixed-tab-and-spaces indentation.

--- types/autounion.hh
+++ types/autounion.hh
@@ -2,49 +2,48 @@
 #define __YAAL_TYPES__AUTOUNION__ 1
 #include "../requirements.hh"
 #ifdef __YAAL__
- 
+
 /* yaal/types/autounion.hh
 */
- 
+
 namespace yaal {
 
     template<typename T, bool bigendian = false>
     class autounion {
-    
+
         union data_u {
-    		T value;
-    		uint8_t bytes[sizeof(T)];
-    		data_u(T const& t): value(t) {}
-    	} data;
-    
+            T value;
+            uint8_t bytes[sizeof(T)];
+            data_u(T const& t): value(t) {}
+        } data;
+
     public:
-    	autounion() {}
-    	autounion(T const& t): data(t) {}
-    
-    
-    	operator const T () const {
-    		return data.value;
-    	}
-    
-    	operator T& () {
-    		return data.value;
-    	}
-    
-    	uint8_t& operator[] (uint8_t const i) {
-    		if (bigendian)
-    			return data.bytes[i];
-    		else
-    			return data.bytes[size - 1 - i];
-    	}
-    
-    	uint8_t operator[] (uint8_t const i) const {
-    		if (bigendian)
-    			return data.bytes[i];
-    		else
-    			return data.bytes[size - 1 - i];
-    	}
-    
-    	static const uint8_t size = sizeof(T);
+        autounion() {}
+        autounion(T const& t): data(t) {}
+
+        operator const T () const {
+            return data.value;
+        }
+
+        operator T& () {
+            return data.value;
+        }
+
+        uint8_t& operator[] (uint8_t const i) {
+            if (bigendian)
+               return data.bytes[i];
+            else
+                return data.bytes[size - 1 - i];
+        }
+
+        uint8_t operator[] (uint8_t const i) const {
+            if (bigendian)
+                return data.bytes[i];
+            else
+                return data.bytes[size - 1 - i];
+        }
+
+        static const uint8_t size = sizeof(T);
     };
 
 }
@@ -54,22 +53,22 @@ namespace yaal {
 
 void fancy(autounion<uint16_t> value) {
     value -= 10;
-	std::cout << value << ":" << std::endl;
-	for (int i = 0; i < value.size; ++i) {
-		std::cout << (int)value[i] << std::endl;
-	}
+    std::cout << value << ":" << std::endl;
+    for (int i = 0; i < value.size; ++i) {
+        std::cout << (int)value[i] << std::endl;
+    }
 }
 
 int main() {
-	fancy(0x00ff);
+    fancy(0x00ff);
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 
-	autounion<uint16_t, false> foo = 0x00ff;
+    autounion<uint16_t, false> foo = 0x00ff;
 
-	for (int i = 0; i < foo.size; ++i) {
-		std::cout << (int)foo[i] << std::endl;
-	}
+    for (int i = 0; i < foo.size; ++i) {
+        std::cout << (int)foo[i] << std::endl;
+    }
 }
 #endif
 
