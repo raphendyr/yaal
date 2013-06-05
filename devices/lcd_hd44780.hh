806fde6d4d900ecb46f6bd936a7e2e23388aaf1c
Ari Sundholm <asundhol@cs.hut.fi>
2013-06-05 12:46:33 +0300

    lcd_hd44780: Get rid of some unnecessary delays in the 4-bit interface.
    There is no need to wait for 50 us between nibble writes and reads.

--- devices/lcd_hd44780.hh
+++ devices/lcd_hd44780.hh
@@ -60,7 +60,7 @@ namespace yaal {
         Interface interface;
 
         uint8_t read_bf_addr() {
-           interface.setRS(false); // Select instruction register.
+           interface.set_RS(false); // Select instruction register.
            return interface.read_fast();
         }
     public:
@@ -89,19 +89,19 @@ namespace yaal {
         }
 
         void clear() {
-            interface.setRS(false); // Select instruction register.
+            interface.set_RS(false); // Select instruction register.
             interface.write(LCD_CLEARDISPLAY);
             _delay_us(2000); // Clearing takes at most 1.52 ms.
         }
 
         void home() {
-            interface.setRS(false); // Select instruction register.
+            interface.set_RS(false); // Select instruction register.
             interface.write(LCD_RETURNHOME);
             _delay_us(2000); // Returning home takes at most 1.52 ms.
         }
 
         void display_control(bool display_on, bool cursor_on, bool blink_on) {
-            interface.setRS(false); // Select instruction register.
+            interface.set_RS(false); // Select instruction register.
             interface.write(LCD_DISPLAYCONTROL |
                             (display_on ? LCD_DISPLAYON : LCD_DISPLAYOFF) |
                             (cursor_on  ? LCD_CURSORON  : LCD_CURSOROFF)  |
@@ -109,31 +109,31 @@ namespace yaal {
         }
 
         void entry_mode(IncDec id, bool shift) {
-            interface.setRS(false); // Select instruction register.
+            interface.set_RS(false); // Select instruction register.
             interface.write(LCD_ENTRYMODESET |
                            (id == INCREMENT ? LCD_ENTRYINCREMENT : LCD_ENTRYDECREMENT) |
                            (shift ? LCD_ENTRYSHIFT : LCD_ENTRYNOSHIFT));
         }
 
         void cursor_display_shift(CurDispShift op, ShiftDirection dir) {
-            interface.setRS(false); // Select instruction register.
+            interface.set_RS(false); // Select instruction register.
             interface.write(LCD_CURSORSHIFT |
                             (op == MOVE_CURSOR ? LCD_CURSORMOVE : LCD_DISPLAYMOVE) |
                             (dir == LEFT ? LCD_MOVELEFT : LCD_MOVERIGHT));
         }
 
         void set_cgram_addr(uint8_t addr) {
-            interface.setRS(false); // Select instruction register.
+            interface.set_RS(false); // Select instruction register.
             interface.write(LCD_SETCGRAMADDR | (addr & ~0xc0));
         }
 
         void set_ddram_addr(uint8_t addr) {
-            interface.setRS(false); // Select instruction register.
+            interface.set_RS(false); // Select instruction register.
             interface.write(LCD_SETDDRAMADDR | (addr & ~0x80));
         }
 
         uint8_t read_char() {
-            interface.setRS(true); // Select data register.
+            interface.set_RS(true); // Select data register.
             return interface.read();
         }
 
@@ -148,7 +148,7 @@ namespace yaal {
         // Puts a character into DDRAM or CGRAM. In the latter case, remember
         // that multiple calls are needed to define a glyph!
         void put_char(uint8_t c) {
-            interface.setRS(true); // Select data register.
+            interface.set_RS(true); // Select data register.
             interface.write(c);
         }
 
@@ -177,12 +177,10 @@ namespace yaal {
         D2 D2_pin;
         D3 D3_pin;
 
-
-        void pulseEnable() {
+        void pulse_enable() {
             enable_pin = true;
             _delay_us(1); // >450 ns is a sufficient pulse width.
             enable_pin = false;
-            _delay_us(50); // >37 us is enough for commands.
         }
 
         void write4(uint8_t value) {
@@ -190,11 +188,11 @@ namespace yaal {
             D1_pin = value & 0x02;
             D2_pin = value & 0x04;
             D3_pin = value & 0x08;
-            pulseEnable();
+            pulse_enable();
         }
 
-        // A 4-bit read without an additional delay.
-        uint8_t read4_fast() {
+        // A 4-bit read.
+        uint8_t read4() {
             enable_pin = true;
             _delay_us(1); // >450 ns is a sufficient pulse width.
                           // This also includes a max 160 ns delay for data.
@@ -206,13 +204,6 @@ namespace yaal {
 
             enable_pin = false;
 
-            return ret;
-        }
-
-        // A 4-bit read with a 50 us delay. Used for most read commands.
-        uint8_t read4() {
-            uint8_t ret = read4_fast();
-            _delay_us(50); // >37 us is enough for commands.
             return ret;
         }
 
@@ -244,33 +235,43 @@ namespace yaal {
             D3_pin = false;
 
             // One.
-            pulseEnable();
-            _delay_us(4500); // >4.1 ms is enough.
+            pulse_enable();
+            _delay_us(4500); // >4.1 ms + >37 us is enough.
 
             // Two.
-            pulseEnable();
-            _delay_us(150); // >100 us is enough.
+            pulse_enable();
+            _delay_us(150); // >100 us + >37 us is enough.
 
             // Three.
-            pulseEnable();
-            // No delay necessary.
+            pulse_enable();
+            _delay_us(50); // >37 us is enough for commands.
 
             // Four: finally set 4-bit mode.
             D0_pin = false;
-            pulseEnable();
+            pulse_enable();
+            _delay_us(50); // >37 us is enough for commands.
 
             return LCD_4BITMODE;
         }
 
-        // An 8-bit write.
+        // An 8-bit write, with a delay to wait for command completion.
         void write(uint8_t value) {
             write4(value >> 4);
             write4(value);
+            _delay_us(50); // >37 us is enough for commands.
         }
 
-        // An 8-bit read, with a delay after each 4-bit read.
-        // Used for most read commands.
+        // An 8-bit read, with a delay to wait for command completion.
+        // Used for other read commands than reading the busy flag.
         uint8_t read() {
+            uint8_t ret = read_fast();
+            _delay_us(50); // >37 us is enough for commands.
+
+            return ret;
+        }
+
+        // An 8-bit read without a delay. Used for reading the busy flag.
+        uint8_t read_fast() {
             D0_pin.mode = INPUT;
             D1_pin.mode = INPUT;
             D2_pin.mode = INPUT;
@@ -289,29 +290,10 @@ namespace yaal {
             return ret;
         }
 
-        // An 8-bit read without a delay.
-        uint8_t read_fast() {
-            D0_pin.mode = INPUT;
-            D1_pin.mode = INPUT;
-            D2_pin.mode = INPUT;
-            D3_pin.mode = INPUT;
-            rw_pin = true;
-
-            uint8_t ret = read4_fast() << 4;
-            ret |= read4_fast() & 0x0f;
-
-            rw_pin = false;
-            D0_pin.mode = OUTPUT;
-            D1_pin.mode = OUTPUT;
-            D2_pin.mode = OUTPUT;
-            D3_pin.mode = OUTPUT;
-
-            return ret;
-        }
-
-        void setRS(bool value) {
+        void set_RS(bool value) {
             rs_pin = value;
         }
+
     };
 }
 
