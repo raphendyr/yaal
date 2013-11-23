f4a29f0dce843a186b7fc91d74d9ec45b41e46e9
Jaakko Kantojärvi <jaakko@n-1.fi>
2013-11-24 00:39:24 +0200

    Support to store strings on program memory.
    Sadly this feature requires macro.

--- util/formating.hh
+++ util/formating.hh
@@ -3,12 +3,34 @@
 #include "../requirements.hh"
 #ifdef __YAAL__
 
+// used by StreamPgmString
+#ifdef __AVR__
+#  include <avr/pgmspace.h>
+#endif
+
 namespace yaal {
 
     namespace internal {
 
         template< typename T >
         struct StreamActor : public T {};
+
+#     ifdef __AVR__
+        struct _StreamPgmString {
+            const char* string;
+
+            template< typename Stream >
+            void operator() (Stream& stream) {
+                const char* s = string;
+                char c;
+                while ( (c = pgm_read_byte(s++)) ) {
+                    // if (c == '\n') *stream << '\r';
+                    stream << c;
+                }
+            }
+        };
+        using StreamPgmString = StreamActor<_StreamPgmString>;
+#     endif
 
         struct _StreamEndline {
             template< typename Stream >
@@ -23,6 +45,20 @@ namespace yaal {
         using StreamEndline = StreamActor<_StreamEndline>;
 
     }
+
+
+    /* ProgramMemory String */
+
+# ifdef __AVR__
+#   define _T(s) (__extension__({ \
+        static const char __attribute__ ((__progmem__,used)) __CONCAT(c_,__LINE__)[] = (s); \
+        ::yaal::internal::StreamPgmString __CONCAT(p_,__LINE__); \
+        __CONCAT(p_,__LINE__).string = __CONCAT(c_,__LINE__); \
+        __CONCAT(p_,__LINE__); \
+    }))
+# else
+#   define _T(s) (s)
+# endif
 
 
     /* Endline */
