c136868b7610a4b6749b7e9e4cb798ba4e13ea74
Jaakko Kantojärvi <jaakko@n-1.fi>
2014-03-12 16:27:32 +0200

    Add type helpers TypesMatch, TypeIfValid and NoType
    TypesMatch is basically same as std::is_same
    NoType is named type to be used to represent that there is no type
    TypeIfValid returns first type if is not NoType otherwise it returns the second

--- types/helpers.hh
+++ types/helpers.hh
@@ -20,6 +20,38 @@ namespace yaal {
             typedef true_type type;
         };
 
+        /*! TypesMatch template struct can be used to check
+         * if two types are the same
+         * ref: std::is_same
+         */
+        template< typename T, typename U >
+        struct TypesMatch {
+            constexpr static bool value = false;
+        };
+        template< typename T >
+        struct TypesMatch<T, T> {
+            constexpr static bool value = true;
+        };
+
+        /*! NoType can be used e.g. with TypesMatch in CRTP inheritance
+         * to detect if argument was given to Derived type
+         */
+        struct NoType {};
+
+
+        /*! TypeIfValid helps to choose a type if it's not NoType
+         * or it will retorn fallback type if it is NoType
+         */
+        template< typename ok_type, typename fallback_type >
+        struct TypeIfValid {
+            typedef typename TypeIf<
+                !TypesMatch<ok_type, NoType>::value,
+                ok_type,
+                fallback_type
+            >::type type;
+        };
+
+
     }
 
 }
